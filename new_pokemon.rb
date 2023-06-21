5c0e3ba4de1c4f2650eb4671ab7aa636d92368bb
Jonathan Chan <jonathan.chan@dynamitegames.io>
2023-06-21 10:17:09 +0800

    Reset back to old commit

--- new_pokemon.rb
+++ new_pokemon.rb
@@ -9,7 +9,7 @@
 class Pokemon
   attr_accessor :name, :hp, :attack, :attack_multiplier, :defense_multiplier, :stun_status, :skill_rng
 
-  def initialize(name, hp, attack, skill_rng) test
+  def initialize(name, hp, attack, skill_rng)
     @name = name
     @hp = hp
     @attack = attack
